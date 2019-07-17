8ac9da886db4e554d94231cef486cdf0d480db59
Annen <jitka.annen@gmail.com>
2019-07-17 09:40:14 +0200

    Update CRS-R_index.R

--- CRS-R_index.R
+++ CRS-R_index.R
@@ -1,4 +1,14 @@
+# Read file with highest CRS-R subscores (with column names Auditory, Visual, Motor, Oromotor, Arousal).
+# Alternatively, a data file with binary information for the presence or absence of every CRS-R item can
+# be provided to calculate the CRS-R index. In that case, data should be organized per CRS-R item in a 
+# binary fashion (0 for absent and 1 for present). Column names should be numbered by the hierachical order
+# of the CRS-R items (Aud.1-Aud.4, Vis.1-Vis.5, Mot.1-Mot.5, Orom.1-Orom.3, Arou.1-Arou.3). Items for a
+# diagnosis of EMCS are excluded.
 
+# mydata = read.csv() 
+
+# Initialize the transposition matrix to calculate the CRS-R index (Annen*, Filippini* et al., (2019)), 
+# same as from CRS-R modified score (Sattin et al., 2015).
 RBvalue = c(1.00, 0.86, 0.71, 0.57, 0.43, 0.29, 0.14, 0.00)
 CMBvalue = c(0.00, 0.09, 0.18, 0.27, 0.36, 0.45, 0.55, 0.64, 0.73, 0.82, 0.91, 1.00)
 
@@ -9,40 +19,47 @@ MSmatrix = matrix(c(7.29, 15.63, 23.97, 32.31, 40.64, 48.98, 57.32, 65.65, 73.99
                     3.13, 11.46, 19.80, 28.14, 36.47, 44.81, 53.15, 61.48, 69.82, 78.16, 86.49, 94.83,
                     2.08, 10.42, 18.76, 27.09, 35.43, 43.77, 52.11, 60.44, 68.78, 77.12, 85.45, 93.79,
                     1.04, 9.38, 17.72, 26.05, 34.39, 42.73, 51.06, 59.40, 67.74, 76.07, 84.41, 92.75,
-                    0.00, 8.34, 16.67, 25.01, 33.35, 41.68, 50.02, 58.36, 66.69, 75.03, 83.37, 91.71),  ncol = 12, nrow = 8, byrow = TRUE)
+                    0.00, 8.34, 16.67, 25.01, 33.35, 41.68, 50.02, 58.36, 66.69, 75.03, 83.37, 91.71),
+                    ncol = 12, nrow = 8, byrow = TRUE)
 
-#If binary data per item in every subscale is provided
-demo_pat$AuditoryCMB = with(demo_pat, ifelse(Aud.4 %in% c(1), 2, ifelse(Aud.3 %in% c(1), 1, 0)))
-demo_pat$AuditoryRB = with(demo_pat, ifelse(Aud.2 %in% c(1), 2, ifelse(Aud.1 %in% c(1), 1, 0)))
-demo_pat$VisualCMB = with(demo_pat, ifelse(Vis.5 %in% c(1), 4, ifelse(Vis.4 %in% c(1), 3, ifelse(Vis.3 %in% c(1), 2, ifelse(Vis.2 %in% c(1), 1, 0)))))
-demo_pat$VisualRB = with(demo_pat, ifelse(Vis.1 %in% c(1), 1, 0))
-demo_pat$MotorCMB = with(demo_pat, ifelse(Mot.5 %in% c(1), 3, ifelse(Mot.4 %in% c(1), 2, ifelse(Mot.3 %in% c(1), 1, 0))))
-demo_pat$MotorRB = with(demo_pat, ifelse(Mot.2 %in% c(1), 2, ifelse(Mot.1 %in% c(1), 1, 0)))
-demo_pat$OromotorCMB = ifelse(Orom.3 %in% c(1), 1, 0)
-demo_pat$OromotorRB = with(demo_pat, ifelse(Orom.2 %in% c(1), 2, ifelse(Orom.1 %in% c(1), 1, 0)))
+# Use this if highest score per subscale is provided. This is appropriate to calculate the 
+# CRS-R index as proposed by Annen*, Filippini* et al., (2019).
+mydata$AuditoryRB = ifelse(mydata$Auditory > 2, 2, mydata$Auditory)
+mydata$AuditoryCMB = ifelse(mydata$Auditory < 3, 0, mydata$Auditory - 2)
+mydata$VisualRB = ifelse(mydata$Visual > 1, 1, mydata$Visual)
+mydata$VisualCMB = ifelse(mydata$Visual < 2, 0, mydata$Visual - 1)
+mydata$MotorRB = ifelse(mydata$Motor > 2, 2, mydata$_Motor)
+mydata$MotorCMB = ifelse(mydata$Motor < 3, 0, mydata$Motor - 3)
+mydata$OromotorRB = ifelse(mydata$Oromotor > 2, 2, mydata$Oromotor)
+mydata$OromotorCMB = ifelse(mydata$Oromotor < 3, 0, mydata$Oromotor - 2)
+mydata$ArousalMS = mydata$Arousal * (1/3)
 
-#If highest score per subscale is provided
-AuditoryRB = ifelse(demo_pat$bestCRS_Auditory > 2, 2, demo_pat$bestCRS_Auditory)
-AuditoryCMB = ifelse(demo_pat$bestCRS_Auditory < 3, 0, demo_pat$bestCRS_Auditory - 2)
-VisualRB = ifelse(demo_pat$best_CRS_visual_function > 1, 1, demo_pat$best_CRS_visual_function)
-VisualCMB = ifelse(demo_pat$best_CRS_visual_function < 2, 0, demo_pat$best_CRS_visual_function - 1)
-MotorRB = ifelse(demo_pat$best_CRS_Motor > 2, 2, demo_pat$best_CRS_Motor)
-MotorCMB = ifelse(demo_pat$best_CRS_Motor < 3, 0, demo_pat$best_CRS_Motor - 3)
-OromotorRB = ifelse(demo_pat$bestCRS_oromotor_verbal > 2, 2, demo_pat$bestCRS_oromotor_verbal)
-OromotorCMB = ifelse(demo_pat$bestCRS_oromotor_verbal < 3, 0, demo_pat$bestCRS_oromotor_verbal - 2)
+# Use this (instead of lines 22-30) if binary data per item in every subscale is provided.
+# mydata$AuditoryCMB = with(mydata, ifelse(Aud.4 %in% c(1), 2, ifelse(Aud.3 %in% c(1), 1, 0)))
+# mydata$AuditoryRB = with(mydata, ifelse(Aud.2 %in% c(1), 2, ifelse(Aud.1 %in% c(1), 1, 0)))
+# mydata$VisualCMB = with(mydata, ifelse(Vis.5 %in% c(1), 4, ifelse(Vis.4 %in% c(1), 3, 
+#                         ifelse(Vis.3 %in% c(1), 2, ifelse(Vis.2 %in% c(1), 1, 0)))))
+# mydata$VisualRB = with(mydata, ifelse(Vis.1 %in% c(1), 1, 0))
+# mydata$MotorCMB = with(mydata, ifelse(Mot.5 %in% c(1), 3, ifelse(Mot.4 %in% c(1), 2, 
+#                        ifelse(Mot.3 %in% c(1), 1, 0))))
+# mydata$MotorRB = with(mydata, ifelse(Mot.2 %in% c(1), 2, ifelse(Mot.1 %in% c(1), 1, 0)))
+# mydata$OromotorCMB = ifelse(Orom.3 %in% c(1), 1, 0)
+# mydata$OromotorRB = with(mydata, ifelse(Orom.2 %in% c(1), 2, ifelse(Orom.1 %in% c(1), 1, 0)))
+# mydata$ArousalMS = with(mydata, ifelse(Arou.3 %in% c(1), 1, ifelse(Arou.2 %in% c(1), 2/3, 
+#                         ifelse(Arou.1 %in% c(1), 1/3, 0))))
 
-demo_pat$ArousalMS = with(demo_pat, ifelse(Eveil.3 %in% c(1), 1, ifelse(Eveil.2 %in% c(1), 2/3, ifelse(Eveil.1 %in% c(1), 1/3, 0))))
 
-demo_pat$RB = with(demo_pat, AuditoryRB + VisualRB + MotorRB + OromotorRB)/7
-demo_pat$CMB = with(demo_pat, AuditoryCMB + VisualCMB + MotorCMB + OromotorCMB)/11
+#Obtain the reflexive behavior and cognitively mediated behavior value by addition of 
+mydata$RB = with(mydata, AuditoryRB + VisualRB + MotorRB + OromotorRB)/7
+mydata$CMB = with(mydata, AuditoryCMB + VisualCMB + MotorCMB + OromotorCMB)/11
 
-demo_pat$index_rb = 0
-demo_pat$index_cmb = 0
-demo_pat$MS_value = 0
-for (i in 1:length(demo_pat$RB)){
-  demo_pat$index_rb[i]  = match(round(demo_pat$RB[i], 2), RBvalue)
-  demo_pat$index_cmb[i] = match(round(demo_pat$CMB[i], 2), CMBvalue)
-  demo_pat$MS_value[i] = MSmatrix[demo_pat$index_rb[i], demo_pat$index_cmb[i]]
+mydata$index_rb = 0
+mydata$index_cmb = 0
+mydata$MS_value = 0
+for (i in 1:length(mydata$RB)){
+  mydata$index_rb[i]  = match(round(mydata$RB[i], 2), RBvalue)
+  mydata$index_cmb[i] = match(round(mydata$CMB[i], 2), CMBvalue)
+  mydata$MS_value[i] = MSmatrix[mydata$index_rb[i], mydata$index_cmb[i]]
 }
 
-demo_pat$CRS-R_index = demo_pat$MS_value + demo_pat$ArousalMS
+mydata$CRS_R_index = mydata$MS_value + mydata$ArousalMS
